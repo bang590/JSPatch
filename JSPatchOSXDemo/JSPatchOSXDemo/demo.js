814168386175358b497994ebc2e36de3be3846e4
Felix Deimel <felix@lemonmojo.com>
2015-05-26 14:26:23 +0200

    Added OS X Demo

---
+++ JSPatchOSXDemo/JSPatchOSXDemo/demo.js
@@ -0,0 +1,21 @@
+defineClass('AppDelegate', {
+    buttonJavaScriptTest_action: function(sender) {
+        self.setClickCount(self.clickCount() + 1);
+        
+        sender.setTitle("Clicked " + self.clickCount() + " times");
+        
+        self.tableView().reloadData();
+    },
+            
+    numberOfRowsInTableView: function(tableView) {
+        return self.clickCount();
+    },
+            
+    tableView_viewForTableColumn_row: function(tableView, tableColumn, row) {
+        var view = tableView.makeViewWithIdentifier_owner("TABLEVIEW_CELL", self);
+        
+        view.textField().setStringValue("Clicked " + (row + 1) + " times");
+        
+        return view;
+    }
+});
