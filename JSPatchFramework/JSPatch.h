15cb8601c54f7a7f3e472e48d8093db4f038f94b
Lex Tang <lexrus@gmail.com>
2015-05-26 10:26:10 +0800

    Fix Travis build error.

--- JSPatchFramework/JSPatch.h
+++ JSPatchFramework/JSPatch.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char JSPatchVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JSPatchFramework/PublicHeader.h>
 
-#import <JSPatch/JSEngine.h>
+#import <JSPatch/JPEngine.h>
