0582a63b9e8594db861592574a2218f1dbfcb05e
qiuweijia <qiuweijia@meituan.com>
2015-06-04 17:12:17 +0800

    add some test for multithread test

---
+++ JSPatchDemo/JSPatchDemo/MultithreadTestObject.h
@@ -0,0 +1,13 @@
+//
+//  MultithreadTestObject.h
+//  JSPatchDemo
+//
+//  Created by Qiu WeiJia on 15/6/4.
+//  Copyright (c) 2015年 bang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MultithreadTestObject : NSObject
+
+@end
---
+++ JSPatchDemo/JSPatchDemo/MultithreadTestObject.m
@@ -0,0 +1,13 @@
+//
+//  MultithreadTestObject.m
+//  JSPatchDemo
+//
+//  Created by Qiu WeiJia on 15/6/4.
+//  Copyright (c) 2015年 bang. All rights reserved.
+//
+
+#import "MultithreadTestObject.h"
+
+@implementation MultithreadTestObject
+
+@end
