afd89ead78c3589a8f92fa8cc0b234138d81a5fc
bang590 <bang590@gmail.com>
2015-11-14 18:35:39 +0800

    Fix compile error

--- Loader/JPLoader.h
+++ Loader/JPLoader.h
@@ -6,17 +6,17 @@
 //  Copyright (c) 2015 bang. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 const static NSString *rootUrl = @"";
 static NSString *publicKey = @"-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+1xcYsEE+ab/Ame1/HHAgfBRh\nD67I9mBYCiOJqC3lJX5RKFvtOTcF5Sf5Bz3NL/2QWPLu40+yt4EvjZ3HOUAHrVgo\n2Fjo4vpaRoEaEtaccOziPH/ASScOfL+uppNGOa0glTCZLKVZI3Go8zoutr8VDw2d\nNT7rDM/4TvPjwMYd3QIDAQAB\n-----END PUBLIC KEY-----";
 
 typedef void (^JPUpdateCallback)(NSError *error);
 
-NS_ENUM(NSInteger) {
+typedef enum {
     JPUpdateErrorUnzipFailed = -1001,
     JPUpdateErrorVerifyFailed = -1002,
-};
+} JPUpdateError;
 
 @interface JPLoader : NSObject
 + (BOOL)run;
