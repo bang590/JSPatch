ab479a07f52ce76482408d3e211b2b6f8dc53bfe
Awhisper <Awhisper@AwhisperdeMBP.lan>
2015-12-27 17:25:28 +0800

    more detail of new protocol test case

--- JSPatchDemo/JSPatchTests/newProtocolTest.js
+++ JSPatchDemo/JSPatchTests/newProtocolTest.js
@@ -1,187 +1,240 @@
 defineProtocol("baseTestProtocol",{
 //argument type can be bool  BOOL int NSInterger float CGFloat ... any base type
-               
+       
 //one argument BOOL  return int
-       testProtocol:{
-       paramsType:"BOOL",
-       returnType:"int",
-       },
-               
-//one argument NSInterger  return void
-       test2Protocol:{
-       paramsType:"BOOL",
-//       returnType:"void",//option no returnType mean void
-       },
-               
-//multiArguments seperated by ","
-       test3Protocol_withB_withC:{
-       paramsType:"BOOL , float , CGFloat",
-       //       returnType:"void",//option no returnType mean void
-       },
+  testProtocol:{
+    paramsType:"BOOL",
+    returnType:"int",
+  },
+       
+  //one argument NSInterger  return void
+    test2Protocol:{
+    paramsType:"BOOL",
+  //returnType:"void",//option no returnType mean void
+  },
+       
+  //multiArguments seperated by ","
+  test3Protocol_withB_withC:{
+    paramsType:"BOOL , float , CGFloat",
+    //returnType:"void",//option no returnType mean void
+  },
 })
+
+//with new protocol  you can add basetype args method
+defineClass('baseTestProtocolObject : NSObject <baseTestProtocol>' , {
+  testProtocol:function(arg1){
+    console.log(arg1);
+    return 1;
+  },
+  test2Protocol: function(arg1) {
+    console.log(arg1);
+  },
+  test3Protocol_withB_withC:function(arg1,arg2,arg3){
+    console.log(arg1);
+  },
+})
+
 
 
 defineProtocol("structTestProtocol",{
 //argument type can be CGRect  CGSize CGPoint ... many systemstruct
 //only support system struct
 //custom struct see  encodeTypeTest
-               
-       //one argument CGRect  return int
-       testProtocol:{
-       paramsType:"CGRect",
-       returnType:"int",
-       },
        
-       //one argument CGSize  return CGPoint
-       test2Protocol:{
-       paramsType:"CGSize",
-       returnType:"CGPoint",
-       },
-       
-       //multiArguments seperated by ","  struct & baseType mix
-       test3Protocol_withB_withC:{
-       paramsType:"CGRect , float , NSInterger",
-       returnType:"CGSize",
-       },
+  //one argument CGRect  return int
+  testProtocol:{
+    paramsType:"CGRect",
+    returnType:"int",
+  },
 
+  //one argument CGSize  return CGPoint
+  test2Protocol:{
+    paramsType:"CGSize",
+    returnType:"CGPoint",
+  },
+
+  //multiArguments seperated by ","  struct & baseType mix
+  test3Protocol_withB_withC:{
+    paramsType:"CGRect , float , NSInteger",
+    returnType:"CGSize",
+  },
+})
+
+//with new protocol  you can add struct type args method
+defineClass('structTestProtocolObject : NSObject <structTestProtocol>' , {
+  testProtocol:function(arg1){
+    console.log(arg1);
+    return 1;
+  },
+  test2Protocol: function(arg1) {
+    console.log(arg1);
+    var point = {x:100,y:100};
+    return point;
+  },
+  test3Protocol_withB_withC:function(arg1,arg2,arg3){
+    console.log(arg1);
+    var size = {width:100,height:100};
+    return size;
+  },
 })
 
 
 defineProtocol("objectTestProtocol",{
 //argument type can be any NSObject
-               
-        //one argument NSString ,  return int
-        //you can turn NSString to id
-        testProtocol:{
-            paramsType:"id",
-            returnType:"int",
-        },
-               
-       //you can use NSString is All ok
-        test2Protocol:{
-            paramsType:"NSString",
-            returnType:"int",
-        },
-               
-        //multiArguments seperated by ","  object & baseType mix
-        test3Protocol_withB_withC:{
-        paramsType:"id , NSString , NSInterger",
-        returnType:"CGSize",
-        },
+     
+  //one argument NSString ,  return int
+  //you can turn NSString to id
+  testProtocol:{
+    paramsType:"id",
+    returnType:"int",
+  },
 
+  //you can use NSString is All ok
+  test2Protocol:{
+    paramsType:"NSString",
+    returnType:"int",
+  },
+
+  //multiArguments seperated by ","  object & baseType mix
+  test3Protocol_withB_withC:{
+    paramsType:"id , NSString , NSInteger",
+    returnType:"CGSize",
+  },
+
+})
+
+//with new protocol  you can add mix type args method
+defineClass('objectTestProtocolObject : NSObject <objectTestProtocol>' , {
+  testProtocol:function(arg1){
+    console.log(arg1);
+    return 1;
+  },
+  test2Protocol: function(arg1) {
+    console.log(arg1);
+    return 1;
+  },
+  test3Protocol_withB_withC:function(arg1,arg2,arg3){
+    console.log(arg1);
+    var size = {width:100,height:100};
+    return size;
+    },
 })
 
 
 
 defineProtocol("specialTestProtocol",{
 //argument type can be selector and block
-               
-    //one argument Selctor  must use "SEL"
-    testProtocol:{
+  //one argument Selctor  must use "SEL"
+  testProtocol:{
     paramsType:"SEL",
-//    returnType:"int",//option no returnType mean void
-    },
+    //returnType:"int",//option no returnType mean void
+  },
 
-    //one argument bloc  must use "block"
-    test2Protocol:{
+  //one argument bloc  must use "block"
+  test2Protocol:{
     paramsType:"block",
-//    returnType:"CGPoint",//option no returnType mean void
-    },
+    // returnType:"CGPoint",//option no returnType mean void
+  },
 
-    //multiArguments seperated by ","  struct & baseType mix
-    test3Protocol_withB_withC:{
+  //multiArguments seperated by ","  struct & baseType mix
+  test3Protocol_withB_withC:{
     paramsType:"CGFloat , block , SEL",
-//    returnType:"CGSize",//option no returnType mean void
-    },
+    // returnType:"CGSize",//option no returnType mean void
+  },
+})
 
+//with new protocol  you can add mix type args method
+defineClass('specialTestProtocolObject : NSObject <specialTestProtocol>' , {
+  testProtocol:function(arg1){
+    console.log(arg1);
+  },
+  test2Protocol: function(arg1) {
+    console.log(arg1);
+    arg1();
+  },
+  test3Protocol_withB_withC:function(arg1,arg2,arg3){
+    console.log(arg1);
+    arg2();
+  },
 })
 
 
 defineProtocol("encodeTestProtocol",{
 //use typeEncode to define complex method
 
-    //you can write typeEncode by using runtime
-    //you can input any string
-    //makesure the number of args  is right
-    testProtocolConstumStruct:{
-        paramsType:"unknown",
-        returnType:"int",
-        typeEncode:"i32@0:8{CGVector=dd}16",
-    },
+  //you can write typeEncode by using runtime
+  //you can input any string
+  //makesure the number of args  is right
+  testProtocol:{
+    paramsType:"unknown",
+    returnType:"something",
+    typeEncode:"v12@0:4@8",
+  },
 
-    //you can write typeEncode by yourself
-    //you can input any string
-    //makesure the number of args  is right
-    testProtocol_withB:{
-        paramsType:"idontknown , mygod",
-        returnType:"wooo",
-        typeEncode:"i@:@^@",
-    },
+  //you can write typeEncode by yourself
+  //you can input any string
+  //makesure the number of args  is right
+  testProtocol_withB:{
+    paramsType:"idontknown , mygod",
+    returnType:"wooo",
+    typeEncode:"@@:@@",
+  },
 
-    //if your paramsType or returnType is complex
-    //youcan input any string just make sure number is right
-    //write the whole typeEncode  or  use runtime to generate typeEncode
+//if your paramsType or returnType is complex
+//youcan input any string just make sure number is right
+//write the whole typeEncode  or  use runtime to generate typeEncode
+})
+
+//with new protocol  you can add encodetype method
+defineClass('typeEncodeTestProtocolObject : NSObject <encodeTestProtocol>' , {
+  testProtocol:function(arg1){
+    console.log(arg1);
+  },
+  test2Protocol_withB: function(arg1,arg2) {
+    console.log(arg1);
+    var ret = "string";
+    return ret;
+  },
 })
 
 
+defineProtocol("classTestProtocol",{},{
+//argument type can be any NSObject
 
-//common testcase
-defineProtocol('lalalala',{
-       testProtocol:{
-           paramsType:"BOOL",
-           returnType:"int",
-       },
-       testProtocol_withB:{
-           paramsType:"id,CGSize",
-           returnType:"NSInteger",
-       },
-       //you can use your classname or id  both is OK
-       testProtocol_withB_withC:{
-           paramsType:"CGRect,float,NSArray",
-           returnType:"id",
-       },
-       //block SEL ok
-       testProtocol_withBlock_withSEL:{
-           paramsType:"CGFloat,block,SEL",
-           returnType:"void",
-       },
-       //some unsupport type  you can use typeEncode
-       //make sure paramsType's number is right
-       testProtocolConstumStruct:{
-           paramsType:"unknown",
-           returnType:"int",
-           typeEncode:"i32@0:8{CGVector=dd}16",//or "i@:{CGVector=dd}"
-       },
-},{
-       xxxxx:{
-               paramsType:"BOOL",
-               returnType:"void",
-       },
-});
+  //one argument NSString ,  return int
+  //you can turn NSString to id
+  testProtocol:{
+    paramsType:"id",
+    returnType:"int",
+  },
 
-//with new protocol  you can add no-id args method
-defineClass('lalalalaViewcontroller:UIViewController <lalalala>', {
-            testProtocolConstumStruct:function(oye){
-                return 6;
-            },
-                testProtocol: function(oye) {
-                return 1;
-            },
-            testProtocol_withB:function(str,size){
-                return 3;
-            },
-            testProtocol_withB_withC:function(rect,f,arr){
-                return "4";
-            },
-            testProtocol_withBlock_withSEL:function(f,b,s){
-                console.log("5")
-            },
-},{
-            xxxxx: function(oye) {
-                console.log("7")
-            },
-            
-});
+  //you can use NSString is All ok
+  test2Protocol:{
+    paramsType:"NSString",
+    returnType:"int",
+  },
 
+  //multiArguments seperated by ","  object & baseType mix
+  test3Protocol_withB_withC:{
+    paramsType:"id , NSString , NSInteger",
+    returnType:"CGSize",
+  },
+})
 
+//with new protocol  you can add class method
+defineClass('classTestProtocolObject : NSObject <classTestProtocol>' ,{}, {
+  testProtocol:function(arg1){
+    console.log(arg1);
+    return 1;
+  },
+  test2Protocol: function(arg1) {
+    console.log(arg1);
+    return 1;
+  },
+  test3Protocol_withB_withC:function(arg1,arg2,arg3){
+    console.log(arg1);
+    var size = {width:100,height:100};
+    return size;
+  },
+})
 
