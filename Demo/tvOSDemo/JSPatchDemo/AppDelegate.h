052765c7c87248846174cba60f3a32272f4ab183
Sergio Padrino <sergio.padrino@gmail.com>
2016-04-23 15:41:07 +0200

    Add demo for tvOS

---
+++ Demo/tvOSDemo/JSPatchDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  JSPatch
+//
+//  Created by bang on 15/4/30.
+//  Copyright (c) 2015年 bang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Demo/tvOSDemo/JSPatchDemo/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSPatch
+//
+//  Created by bang on 15/4/30.
+//  Copyright (c) 2015年 bang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
